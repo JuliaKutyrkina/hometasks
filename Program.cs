d48d2c97c944bf60b77a8de9fea99736ecc224fe
Julia Kutyrkina <u_kutyrkina@mail.ru>
2022-10-06 22:37:15 +0300

    протестированная версия

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// №15, третья цифра произвольного числа
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n>-100 && n<=99)
+    Console.WriteLine("Третья цифра отсутствует");
+else
+{
+   if   (n<-99)
+   {
+    string str = n.ToString();
+    Console.WriteLine(str[3]);
+   }
+    
+   else
+   {
+    string str = n.ToString();
+    Console.WriteLine(str[2]);
+   }
+}
