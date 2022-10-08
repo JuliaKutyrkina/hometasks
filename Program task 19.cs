b8c0e5c330ff279ff3d6ee1ccb04b4d977ba9670
JuliaKutyrkina <114148500+JuliaKutyrkina@users.noreply.github.com>
2022-10-09 01:39:28 +0300

    Add files via upload

---
+++ Program task 19.cs
@@ -0,0 +1,12 @@
+﻿// Задача 19. Проверка числа на палиндром. 
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = n/10000;
+int b = (n%10000)/1000;
+int c = (n%100)/10;
+int d = n%10;
+if (a == d && b == c)
+    Console.WriteLine("Число является палиндромом");
+else
+    Console.WriteLine("Число не является палиндромом");
---
+++ Program task 21.cs
@@ -0,0 +1,17 @@
+﻿//Задача 21. Нахождение расстояния между двумя точками в трехмерном пространстве. 
+Console.Clear();
+Console.Write("Введите координату х1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату у1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату х2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату у2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z2: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(Math.Round(Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2)), 2));
