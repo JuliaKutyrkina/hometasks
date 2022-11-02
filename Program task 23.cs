72461462f154582990ec3e19c7694a802f4f819e
JuliaKutyrkina <114148500+JuliaKutyrkina@users.noreply.github.com>
2022-11-02 23:59:41 +0300

    Delete Program task 23.cs

--- Program task 23.cs
+++
@@ -1,6 +0,0 @@
-﻿//Задача 23. Нахождение кубов всех числе от 1 до заданного N.
-Console.Clear();
-Console.Write("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-for (int i = 1; i <= n; i++)
-Console.Write($"{i * i * i} ");
