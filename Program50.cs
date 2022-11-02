ba342cdf9d27debc6f2aa05599ca6477264fc5e8
JuliaKutyrkina <114148500+JuliaKutyrkina@users.noreply.github.com>
2022-11-03 00:03:40 +0300

    Add files via upload

---
+++ Program50.cs
@@ -0,0 +1,32 @@
+﻿// задача 50. Поиск элемента по его позиции в двумерном массиве. 
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(-10, 11);
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+Console.Clear();
+int[,] matrix = new int[5, 5];
+InputMatrix(matrix);
+Console.WriteLine("Начальный массив: ");
+PrintMatrix(matrix);
+Console.Write("Введите номер строки: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n>5 || m>5 || n<1 ||m<1) Console.Write("Такого элемента не существует, повторите ввод");
+else Console.WriteLine(matrix[n-1,m-1]);
---
+++ Program52.cs
@@ -0,0 +1,37 @@
+﻿// задача 52. Найти среднеарифметическое каждого столбца в двумерном массиве
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ColsAvg (int[,] matrix)
+{
+    for (int j = 0; j< matrix.GetLength(1); j++)
+    {
+        double ColsAvg = 0;
+        for (int i = 0;i < matrix.GetLength(0); i++)
+        {                  
+            ColsAvg += matrix[i,j];
+        }
+        ColsAvg = Math.Round(ColsAvg / matrix.GetLength(0),2);
+        Console.Write("Среднеарифметическое столбца {0}: {1}", j, ColsAvg);         
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите количество строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+InputMatrix(matrix);
+ColsAvg(matrix);
