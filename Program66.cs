275b32c566cfae20133d4b2f3637aa2d0dfbf989
JuliaKutyrkina <114148500+JuliaKutyrkina@users.noreply.github.com>
2022-11-17 22:15:30 +0300

    Add files via upload

---
+++ Program66.cs
@@ -0,0 +1,17 @@
+﻿//66. Введите числа m и n. Просчитать сумму чисел, лежащих в промежутке между ними. 
+
+int ResultNumbers(int m, int n)
+{
+    if (m == n)
+        return m;
+    if (m<n)
+        return ResultNumbers(m + 1, n) + m;
+    return ResultNumbers (m-1, n)+m;
+}
+
+Console.Clear();
+Console.WriteLine(" Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма чисел в промежутке от {m} до {n} составляет: {ResultNumbers(m, n)}");
---
+++ Program68.cs
@@ -0,0 +1,29 @@
+﻿//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+//m = 3, n = 2 -> A(m,n) = 29
+
+int AkkFunction(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m>0 && n == 0)
+        return AkkFunction(m - 1, 1);
+    else 
+        return AkkFunction(m - 1, AkkFunction(m, n - 1));
+}
+
+Console.Clear();
+Console.WriteLine("Введите неотрицательное число m:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите неотрицательное число n:");
+int n = Convert.ToInt32(Console.ReadLine());
+while (m < 0 || n < 0)
+{
+    Console.WriteLine("Введите неотрицательное число m:");
+    m = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите неотрицательное число n:");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine($"Функция Аккермана для чисел A({m},{n}) = {AkkFunction(m, n)}");
